8ca9bdc337a79ab9b626fada09fed4b08f7069f4
Dmitry Morozov <9984398@gmail.com>
2025-04-07 22:38:50 +0300

    --> Home_task 3

--- menu.py
+++ menu.py
@@ -14,15 +14,30 @@ def print_demo_menu(menu_list: dict[str, int]) -> None:
 
 
 def add_menu_item(menu_target: dict[str, int], name: str, price: int) -> bool:
+    if menu_target.get(name):
+        print('Already exists!')
+        return False
+
     menu_target[name] = price
     return True
 
 
-print_demo_menu(menu)
-add_menu_item(menu, 'Bread', 100)
-print_demo_menu(menu)
+def get_new_product():
+    _product = input('Enter product name: ')
+    _price = input('Enter product price: ')
+    return _product, _price
+
+
+if __name__ == '__main__':
+    # print_demo_menu(menu)
+    # add_menu_item(menu, 'Bread', 100)
+    # add_menu_item(menu, 'Milk', 200)
+    # add_menu_item(menu, 'Bread', 100)
+    # print_demo_menu(menu)
+    product, price = get_new_product()
+    print(f'Product name: {product} price: {price}')
 
 """
-1) Check in function add_menu_item that item doesnt exist/ Return False if does 
-2) Add function get_new_product() that ask user to input name and price of a new Product and returns a Pair ( name, price )
+1) Write function delete_product ( menu , product_name ) that deletes menu item with product name
+2) Write function that solve and print all roots for math equation like: ax**2 + bx + c = 0  
 """
